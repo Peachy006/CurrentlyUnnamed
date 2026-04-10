a6082dffce9e08227f653ae7d82bc1d4f54e82dc
Peachy006 <seanpettas@gmail.com>
2026-04-10 17:24:19 +0200

    added finished veil and made sure that the routing works

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,28 @@
+import Plasma from "./extras/Plasma"
+import NavBar from "../components/navbar";
+import MobileNavBar from "@/app/components/MobileNavBar";
+
 export default function AboutPage() {
     return(
         <div>
-
+            <div style={{ position: 'fixed', inset: 0, zIndex: 0 }}>
+                <Plasma
+                    color="#ff6b35"
+                    speed={0.2}
+                    direction="forward"
+                    scale={1.4}
+                    opacity={0.4}
+                    mouseInteractive={false}
+                />
+            </div>
+            <div className="relative z-10 w-full flex flex-col items-center">
+                <div className="block min-[801px]:hidden">
+                    <MobileNavBar />
+                </div>
+                <div className="hidden min-[801px]:block">
+                    <NavBar />
+                </div>
+            </div>
         </div>
     )
 }
