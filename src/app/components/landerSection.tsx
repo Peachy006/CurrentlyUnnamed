75543c2e32afb434b28edd2fdcc747c5299eaabe
Peachy006 <seanpettas@gmail.com>
2026-04-14 01:11:43 +0200

    added more responsive design and made space for a png on the right side of the text on the landerSection for desktop view

--- src/app/components/landerSection.tsx
+++ src/app/components/landerSection.tsx
@@ -2,21 +2,27 @@ import BlurText from "../extras/BlurText";
 
 export default function landerSection() {
     return (
-        <div className="w-screen h-[90vh] flex flex-col items-center justify-center text-center">
-            <BlurText
-                text="The Tech Gap"
-                delay={200}
-                animateBy="words"
-                direction="top"
-                className="text-7xl mb-6 font-oswald text-center"
-            />
-            <BlurText
-                text="IT - By humans. For humans"
-                delay={100}
-                animateBy="words"
-                direction="top"
-                className="text-5xl mb-8 pt-2 font-oswald text-center text-neutral-600 border-t-2"
-            />
+        <div className="w-screen h-[90vh] flex flex-row items-center justify-center">
+            <div className="h-full w-1/2 max-xl:w-full flex flex-col items-start justify-center pl-40 max-xl:p-0 max-xl:items-center text-start max-xl:text-center">
+                <BlurText
+                    text="The Tech Gap"
+                    delay={200}
+                    animateBy="words"
+                    direction="top"
+                    className="text-7xl mb-6 font-oswald"
+                />
+                <BlurText
+                    text="IT - By humans. For humans"
+                    delay={100}
+                    animateBy="words"
+                    direction="top"
+                    className="text-5xl mb-8 pt-2 font-oswald text-neutral-600"
+                />
+            </div>
+            <div className="h-full w-1/2 max-xl:hidden">
+
+            </div>
+
         </div>
     );
 }
